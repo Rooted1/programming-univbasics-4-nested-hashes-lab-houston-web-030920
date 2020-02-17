6c766b5ddba6729976e24d6e700b97f210adf16a
Ruth Obe <ruthobey@gmail.com>
2020-02-17 00:04:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,8 +138,8 @@ def adding_to_dennis
         }
      }
      
-    programmer_hash[:dennis_ritchie][:languages] => "Assembly"
+    # programmer_hash[:dennis_ritchie][:languages] => "Assembly"
      
-     return programmer_hash[:dennis_ritchie][:languages]
+    # return programmer_hash[:dennis_ritchie][:languages]
 
 end
